eec4d904c84f2f900b98abe2008f75ee6172a00f
RenikindiPalanibhavesh <83402834+RenikindiPalanibhavesh@users.noreply.github.com>
2021-05-18 12:47:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
     var n1 = Number(req.body.num1);
-    var n2 = Number(req.body.num2)
+    var n2 = Number(req.body.num2);
     var result = n1 + n2;
     res.send("The answer is " + result);
     
 });
 
 app.listen(3000, function () {
-    console.log("Server has started on port 3000")
+    console.log("Server has started on port 3000");
 });
